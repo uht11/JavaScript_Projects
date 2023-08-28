fc471082b97a08719f6a8c6c35ba9246c7fd59a9
Uzzul Hussain <38548632+uht11@users.noreply.github.com>
2023-08-28 14:00:58 -0400

    feat: making the roll button work and displaying the dice number

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,1 +1,35 @@
 'use strict';
+//Global variables
+let currentScore = 0
+
+
+//getting Players
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+
+//getting score elements
+const score0Elm = document.getElementById('score--0');
+const score1Elm = document.getElementById('score--1');
+const diceElm = document.querySelector('.dice');
+
+//getting buttons
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
+score0Elm.textContent = 0;
+score1Elm.textContent = 0;
+diceElm.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const diceScore = Math.trunc(Math.random() * 6) + 1;
+  console.log(`rolled ${diceScore}`);
+  diceElm.classList.remove('hidden');
+  diceElm.src = `dice-${diceScore}.png`;
+
+  if (diceScore !== 1) {
+
+  }else{
+
+  }
+});
