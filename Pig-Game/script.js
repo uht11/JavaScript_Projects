8d654f624a1d56f90debb7fe42eee1428040c2d3
Uzzul Hussain <38548632+uht11@users.noreply.github.com>
2023-08-28 16:10:39 -0400

    feat: added total score feature and active player switch

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,15 +1,18 @@
 'use strict';
 //Global variables
-let currentScore = 0
-
+let currentScore = 0;
+const totalScore = [0, 0];
 
 //getting Players
+let activePlayer = 0;
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
 
 //getting score elements
 const score0Elm = document.getElementById('score--0');
+let current0Elm = document.getElementById('current--0');
 const score1Elm = document.getElementById('score--1');
+let current1Elm = document.getElementById('current--1');
 const diceElm = document.querySelector('.dice');
 
 //getting buttons
@@ -28,8 +31,25 @@ btnRoll.addEventListener('click', function () {
   diceElm.src = `dice-${diceScore}.png`;
 
   if (diceScore !== 1) {
-
-  }else{
-
+    currentScore += diceScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
   }
+});
+
+btnHold.addEventListener('click', function () {
+  totalScore[activePlayer] += currentScore;
+  score0Elm.textContent = totalScore[0];
+  score1Elm.textContent = totalScore[1];
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
 });
