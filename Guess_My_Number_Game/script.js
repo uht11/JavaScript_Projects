925af72f4c473428c0b0ed7f90b40d537e75333b
Uzzul Hussain <38548632+uht11@users.noreply.github.com>
2023-08-27 22:19:44 -0400

    feat: Updated to make again button and other usecase work

--- Guess_My_Number_Game/script.js
+++ Guess_My_Number_Game/script.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highScore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
-  document.querySelector('.number').textContent = secretNumber;
 
   console.log('Guessed number:', guess);
   if (!guess) {
@@ -18,8 +16,14 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       'WOOHOO, You Guessed Right!!!';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High, Try Again...';
@@ -36,4 +40,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
   document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
 });
